24b4a1aea940c7df65bf0f9c1e2d2738b7a53e92
Natalia Stoyanova <qa.natalia.ruseva@gmail.com>
2021-10-16 19:32:33 +0300

    Zadanie 2. v2.

--- GroupCreation Tests.cs
+++ GroupCreation Tests.cs
@@ -16,6 +16,8 @@ namespace WebAddressbookTests
         private IWebDriver driver;
         private StringBuilder verificationErrors;
         private string baseURL;
+        private bool acceptNextAlert;
+
         //private bool acceptNextAlert = true;
 
         [SetUp]
@@ -73,5 +75,53 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("group page")).Click();
         }
 
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+
+        }
     }
 }
