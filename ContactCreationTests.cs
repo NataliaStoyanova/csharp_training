240c8cbcc75674385ee858839b849dd77dcb27d8
Natalia Stoyanova <qa.natalia.ruseva@gmail.com>
2021-10-19 21:20:58 +0300

    Zadanie 4.

---
+++ ContactCreationTests.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+
+
+        {
+
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToContactsPage();
+
+            ContactData contact = new ContactData("Natalia", "Stoyanova", "08968756");
+            //contact.Firstname = "Natalia";
+              
+            contact.Middlename= "Yurieva";
+            //contact.Lastname = "Stoyanova";
+            contact.Nickname = "Nt_St";
+            contact.Photo= @"C:\Users\stani\OneDrive\Desktop\1.png";
+            contact.Title = "Mrs";
+            contact.Company = "Soft2Run";
+            contact.Address = "Sofia";
+            contact.Home = "099345435";
+            //contact.Mobile = "08968756";
+            contact.Work = "03345346";
+            contact.Fax = "9887665";
+            contact.Email = "qa.natalia.ruseva@gmail.com";
+            contact.Email2 = "qa2.natalia.ruseva@gmail.com";
+            contact.Email3 = "qa3.natalia.ruseva@gmail.com";
+            contact.Homepage = "https://google.com";
+            contact.Bday = "4";
+            contact.Bmonth = "July";
+            contact.Byear = "1985";
+            contact.Aday = "29";
+            contact.Amonth = "July";
+            contact.Ayear = "2018";
+            contact.Address2 = "Tsarevo";
+            contact.Phone2 = "09898786";
+            contact.Notes = "notes";
+
+
+
+        FillContactForm(contact);
+            SubmitNewContact();
+            GoToHomePageContactsTable();
+        }
+
+        private void GoToHomePageContactsTable()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+            driver.FindElement(By.XPath("//table[@id='maintable']/tbody/tr[2]/td[2]")).Click();
+        }
+
+        private void SubmitNewContact()
+        {
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
+        }
+
+        private void FillContactForm(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(contact.Nickname);
+            driver.FindElement(By.Name("photo")).Clear();
+            driver.FindElement(By.Name("photo")).SendKeys(contact.Photo);
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(contact.Title);
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(contact.Company);
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contact.Address);
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(contact.Home);
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contact.Mobile);
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(contact.Work);
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(contact.Fax);
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(contact.Email);
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(contact.Email2);
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(contact.Email3);
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(contact.Homepage);
+            driver.FindElement(By.Name("bday")).Click();
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(contact.Bday);
+            driver.FindElement(By.Name("bmonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(contact.Bmonth);
+            driver.FindElement(By.Name("byear")).Click();
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(contact.Byear);
+            driver.FindElement(By.Name("aday")).Click();
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText(contact.Aday);
+            driver.FindElement(By.Name("amonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText(contact.Amonth);
+            driver.FindElement(By.Name("ayear")).Click();
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys(contact.Ayear);
+            driver.FindElement(By.Name("address2")).Clear();
+            driver.FindElement(By.Name("address2")).SendKeys(contact.Address2);
+            driver.FindElement(By.XPath("//div[@id='content']/form/label[24]")).Click();
+            driver.FindElement(By.Name("phone2")).Click();
+            driver.FindElement(By.Name("phone2")).Clear();
+            driver.FindElement(By.Name("phone2")).SendKeys(contact.Phone2);
+            driver.FindElement(By.Name("notes")).Click();
+            driver.FindElement(By.Name("notes")).Clear();
+            driver.FindElement(By.Name("notes")).SendKeys(contact.Notes);
+        }
+
+        private void GoToContactsPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"Submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+
+            driver.Navigate().GoToUrl(baseURL + "/addressbook");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ ContactData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string nickname;
+        private string photo;
+        private string title;
+        private string company;
+        private string address;
+        private string home;
+        private string mobile;
+        private string work;
+        private string fax;
+        private string email;
+        private string email2;
+        private string email3;
+        private string homepage;
+        private string bday;
+        private string bmonth;
+        private string byear;
+        private string aday;
+        private string amonth;
+        private string ayear;
+        private string address2;
+        private string phone2;
+        private string notes;
+
+        public ContactData(string firstname, string lastname, string mobile)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.mobile = mobile;
+        }
+
+        public ContactData(string firstname, string middlename, string lastname, string nickname, string photo, string title, string company, string address, string home, string mobile, string work, string fax, string email, string email2, string email3, string homepage, string bday, string bmonth, string byear, string aday, string amonth, string ayear, string address2, string phone2, string notes)
+        {
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.lastname = lastname;
+            this.nickname = nickname;
+            this.photo = photo;
+            this.title = title;
+            this.company = company;
+            this.address = address;
+            this.home = home;
+            this.mobile = mobile;
+            this.work = work;
+            this.fax = fax;
+            this.email = email;
+            this.email2 = email2;
+            this.email3 = email3;
+            this.homepage = homepage;
+            this.bday = bday;
+            this.bmonth = bmonth;
+            this.byear = byear;
+            this.aday = aday;
+            this.amonth = amonth;
+            this.ayear = ayear;
+            this.address2 = address2;
+            this.phone2 = phone2;
+            this.notes = notes;
+        }
+
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string Middlename { get => middlename; set => middlename = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public string Nickname { get => nickname; set => nickname = value; }
+        public string Photo { get => photo; set => photo = value; }
+        public string Title { get => title; set => title = value; }
+        public string Company { get => company; set => company = value; }
+        public string Address { get => address; set => address = value; }
+        public string Home { get => home; set => home = value; }
+        public string Mobile { get => mobile; set => mobile = value; }
+        public string Work { get => work; set => work = value; }
+        public string Fax { get => fax; set => fax = value; }
+        public string Email { get => email; set => email = value; }
+        public string Email2 { get => email2; set => email2 = value; }
+        public string Email3 { get => email3; set => email3 = value; }
+        public string Homepage { get => homepage; set => homepage = value; }
+        public string Bday { get => bday; set => bday = value; }
+        public string Bmonth { get => bmonth; set => bmonth = value; }
+        public string Byear { get => byear; set => byear = value; }
+        public string Aday { get => aday; set => aday = value; }
+        public string Amonth { get => amonth; set => amonth = value; }
+        public string Ayear { get => ayear; set => ayear = value; }
+        public string Notes { get => notes; set => notes = value; }
+        public string Phone2 { get => phone2; set => phone2 = value; }
+        public string Address2 { get => address2; set => address2 = value; }
+    }
+}
